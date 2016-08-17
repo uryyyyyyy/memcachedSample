591f716d4e086566778dd708f6d560d701837265
uryyyyyyy <koki305@gmail.com>
2016-08-18 02:34:40 +0900

    elastiCache

--- client/elastiCache/src/test/scala/com/github/uryyyyyyy/memcached/client/elasticache/CodecTest.scala
+++ client/elastiCache/src/test/scala/com/github/uryyyyyyy/memcached/client/elasticache/CodecTest.scala
@@ -6,11 +6,22 @@ import org.scalatest.{FunSpec, MustMatchers}
 class CodecTest extends FunSpec with MustMatchers {
 
   describe("Codec") {
-    it("can encode&decode 1") {
+    it("can encode&decode long value") {
       val obj = 100L
       val bytes = Codec.encode(obj)
       val result = Codec.decode(bytes, classOf[Long])
       result mustBe obj
     }
+
+    it("can encode&decode custom value") {
+      val obj = Seq(ParentObj(ChildObj(1L, 2), "hello"), ParentObj(ChildObj(5L, 9), "hessllo"))
+      val bytes = Codec.encode(obj)
+      val result = Codec.decode(bytes, classOf[Seq[ParentObj]])
+      result mustBe obj
+    }
   }
 }
+
+case class ChildObj(ll: Long, ii: Int)
+
+case class ParentObj(child1: ChildObj, str: String)
